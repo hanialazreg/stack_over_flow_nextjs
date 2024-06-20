56f12f72223850e4cfeb706066763caf7c6ea323
hanialazreg <lazreghania@gmail.com>
2024-06-20 15:55:56 +0100

    sidebar

---
+++ components/shared/RightSidebar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RightSidebar = () => {
+  return <div>this is te right side bar </div>;
+};
+
+export default RightSidebar;
--- components/shared/navbar/MobileNav.tsx
+++ components/shared/navbar/MobileNav.tsx
@@ -3,14 +3,39 @@ import Image from "next/image";
 
 import {
   Sheet,
+  SheetClose,
   SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import Link from "next/link";
+import { SignedOut } from "@clerk/nextjs";
+import { Button } from "@/app/ui/button";
+import { sidebarLinks } from "@/constants";
+
+const NavContent = () => {
+  return (
+    <section className="flex h-full flex-col gap-6 pt-16">
+      {sidebarLinks.map((item) => {
+        return (
+          <SheetClose asChild key={item.route}>
+            <Link href={item.route} className="">
+              <Image
+                src={item.imgURL}
+                alt={item.label}
+                width={20}
+                height={20}
+              />
+              <p>{item.label}</p>
+            </Link>
+          </SheetClose>
+        );
+      })}
+    </section>
+  );
+};
 
 const MobileNav = () => {
+  // mobile dev is desined for responsible design
   return (
     <Sheet>
       <SheetTrigger asChild>
@@ -19,17 +44,50 @@ const MobileNav = () => {
           alt="Menu"
           width={36}
           height={36}
-          className="invert-colors"
+          className="invert-colors
+          sm:hidden"
         />
       </SheetTrigger>
-      <SheetContent>
-        <SheetHeader>
-          <SheetTitle>Are you absolutely sure?</SheetTitle>
-          <SheetDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
-          </SheetDescription>
-        </SheetHeader>
+      <SheetContent
+        side="left"
+        className="background-light900_dark200 border-none"
+      >
+        <Link href="/" className="flex items-center gap-1">
+          <Image
+            src="/assets/images/site-logo.svg"
+            width={23}
+            height={23}
+            alt="DevFlow"
+          />
+
+          <p className="h2-bold text-dark100_light900 font-spaceGrotesk">
+            Dev <span className="text-primary-500">Overflow</span>
+          </p>
+        </Link>
+        <div>
+          <SheetClose asChild>
+            <NavContent />
+          </SheetClose>
+          <SignedOut>
+            <div className="flex flex-col gap-3">
+              <SheetClose asChild>
+                <Link href="/sign-in">
+                  <Button className=" small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+                    <span className=" primary-text-gradient">Log In</span>
+                  </Button>
+                </Link>
+              </SheetClose>
+
+              <SheetClose asChild>
+                <Link href="/sign-up">
+                  <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+                    Sign Up
+                  </Button>
+                </Link>
+              </SheetClose>
+            </div>
+          </SignedOut>
+        </div>
       </SheetContent>
     </Sheet>
   );
