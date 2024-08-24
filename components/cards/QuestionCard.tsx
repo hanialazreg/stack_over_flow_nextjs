46262d97e85dae1050c797c0999ffa84b12544c3
hanialazreg <lazreghania@gmail.com>
2024-08-24 17:52:13 +0100

    home page completed

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import React from "react";
 import { Button } from "../ui/button";
 import RenderTag from "../shared/RenderTag";
+import Metric from "../shared/Metric";
+import { formatAndDivideNumber, getTimestamp } from "@/lib/utils";
 
 interface QuestionProps {
   _id: string;
@@ -32,7 +34,7 @@ const QuestionCard = ({
       <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
         <div>
           <span className="subtle-regular text-dark400_light700 line-clamp-1 flex sm:hidden">
-            {String(createdAt)}
+            {getTimestamp(createdAt)}
           </span>
           <Link href={`/question/${_id}`}>
             <h3 className="sm:h3-semibold base-semibold text-dark200_light900 line-clamp-1 flex-1">
@@ -47,7 +49,38 @@ const QuestionCard = ({
           <RenderTag key={tag._id} _id={tag._id} name={tag.name} />
         ))}
       </div>
-      <div className="flex-between mt-6 w-full flex-wrap gap-3"></div>
+      <div className="flex-between mt-6 w-full flex-wrap gap-3">
+        <Metric
+          imgUrl="/assets/icons/avatar.svg"
+          alt="user"
+          value={author.name}
+          title={` .asked ${getTimestamp(createdAt)}`}
+          href={`/profile/${author._id}`}
+          isAuthor
+          textStyles="body-medium text-dark400_light800"
+        />
+        <Metric
+          imgUrl="/assets/icons/like.svg"
+          alt="upVotes"
+          value={formatAndDivideNumber(upvotes)}
+          title="votes"
+          textStyles="small-medium text-dark400_light800"
+        />
+        <Metric
+          imgUrl="/assets/icons/message.svg"
+          alt="message"
+          value={formatAndDivideNumber(answers.length)}
+          title="answers"
+          textStyles="small-medium text-dark400_light800"
+        />
+        <Metric
+          imgUrl="/assets/icons/eye.svg"
+          alt="views"
+          value={views}
+          title="views"
+          textStyles="small-medium text-dark400_light800"
+        />
+      </div>
     </div>
   );
 };
