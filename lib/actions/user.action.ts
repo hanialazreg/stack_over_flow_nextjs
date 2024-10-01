167acac1271f8b58d00a06a47861bcd0059a2044
hanialazreg <lazreghania@gmail.com>
2024-10-01 17:56:42 +0100

    fix auth

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -15,7 +15,7 @@ export async function getUserById(params: any) {
   try {
     connectToDatabase();
     const { userId } = params;
-    const user = await User.findOne({ clecrkId: userId });
+    const user = await User.findOne({ clerkId: userId });
     return user;
   } catch (error) {
     console.log(error);
